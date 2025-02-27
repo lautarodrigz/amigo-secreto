7b6cdf21605a2f8347f47762a5df96dc4e9b9919
lautarodrigz <lautirod12@gmail.com>
2025-02-27 18:43:36 -0300

    Add agregarAmigo() function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nuevoAmigo = document.querySelector("#amigo").value;
+    if (nuevoAmigo !== "") {
+        amigos.push(nuevoAmigo);
+    } else {
+        alert("Por favor, inserte un nombre");
+    }
+}
